562e58efc816328ba2d8950eb00026cea5950da4
Jeremy Miller <Jeremy.Miller@sluhn.org>
2023-03-29 14:31:57 -0400

    Added Cassia to the Project

--- SNMP_TEST/citrix_server.cs
+++ SNMP_TEST/citrix_server.cs
@@ -11,13 +11,17 @@ namespace SNMP_TEST
     internal class citrix_server
     {
         //attributes
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string citrix_server_name { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         
         public bool PingHost ()
         {
             bool pingable = false;
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
             Ping pinger = null;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
             try
             {
